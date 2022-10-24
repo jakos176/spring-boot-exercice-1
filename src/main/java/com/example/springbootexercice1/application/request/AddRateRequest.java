5011081f876200ef21e96608e4fed9c7f728bfdd
edu <edu@>
2022-10-24 17:12:16 +0200

    pequenyo arreglo

--- src/main/java/com/example/springbootexercice1/application/request/AddRateRequest.java
+++ src/main/java/com/example/springbootexercice1/application/request/AddRateRequest.java
@@ -3,13 +3,15 @@ package com.example.springbootexercice1.application.request;
 import com.example.springbootexercice1.domain.model.Rate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class AddRateRequest {
 
     @NotNull
-    private final Rate rate;
+    private Rate rate;
 }
